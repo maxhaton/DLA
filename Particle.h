3ddece5af3803d1eda8daeddffd7195cbc814415
Max Haughton <mhaughton@symmetryinvestments.com>
2023-02-14 18:00:04 +0000

    The start (may not be at some point if I get annoyed)

---
+++ Particle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <array>
+class Particle {
+  public:
+    static const int dim = 2;  // we are in two dimensions
+    std::array<double, 2> pos;
+  
+    // default constructor
+    Particle() {
+
+    }
+    // constructor, with a specified initial position
+    Particle(double set_pos[]) {
+      this->pos[0] = set_pos[0];
+      this->pos[1] = set_pos[1];
+    }
+};
